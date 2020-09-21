3bef7f806aa9fe75b625fab26481738a21178815
EmisoraSanPablo <estereosanpablo@gmail.com>
2020-09-20 22:59:45 -0500

    Primer commit

---
+++ server/sockets/socket.js
@@ -0,0 +1,53 @@
+const { io } = require('../server');
+const { Usuarios } = require('../classes/usuarios');
+const { crearMensaje } = require('../utilidades/utilidades');
+
+const usuarios = new Usuarios();
+
+io.on('connection', (client)=> {
+
+    client.on('entrarChat', (data, callback) => {
+        // console.log(data.nombre);
+        if (!data.nombre || !data.sala) {
+            return callback({
+                error: true,
+                mensaje: 'El nombre/sala es necesario'  
+            });
+        }
+        // console.log(data.sala);
+        // metodo join() sirve para unir al usuario a una sala
+        client.join(data.sala)
+
+        usuarios.agregaPersona(client.id, data.nombre, data.sala);
+
+        client.broadcast.to(data.sala).emit('listsPersonas', usuarios.getPersonasPorSala(data.sala));
+
+        callback(usuarios.getPersonasPorSala(data.sala));
+    });
+
+    client.on('crearMensaje', (data) => {
+
+        let persona = usuarios.getPersona(client.id);
+
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+        client.broadcast.to(persona.data).emit('crearMensaje', mensaje);
+    });
+
+    client.on('disconnect', () => {
+
+        let personaBorrada = usuarios.borrarPersona(client.id);
+        // console.log(personaBorrada)
+        client.broadcast.to(personaBorrada.sala).emit( 'crearMensaje', crearMensaje(`Administrador: ${personaBorrada.nombre} salio`))
+        client.broadcast.to(personaBorrada.sala).emit( 'listarPersona', usuarios.getPersonasPorSala(personaBorrada.sala))
+    });
+
+   
+    // Mensaje privado
+    // Metodo to() nos permite enviar un mensaje privado, ingresandole un ID
+    client.on('mensajePrivado', data => {
+        let persona = usuarios.getPersona(client.id);
+
+        client.broadcast.to(data.para).emit('mensajePrivado', crearMensaje(persona.nombre, data.mensaje));
+    })
+    
+});
